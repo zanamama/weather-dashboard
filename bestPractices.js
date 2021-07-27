ffa45249e2a6db6e609d951c23914d81725125fb
zanama <57731386+zanmama@users.noreply.github.com>
2021-07-27 19:31:46 -0400

    changed buttons and reformatted html page to match model

---
+++ bestPractices.js
@@ -0,0 +1,20 @@
+// Best Practices 101
+// Create this folders
+//  Project folder
+//      assests
+//          css
+//          images
+//          js
+//      index.html
+//      Readme.md
+
+// On html file, you should only have this 3 element tags as siblings
+// <header>
+//      every header related elements can go here
+// </header>
+// <main class="container">
+//      every main related elements can go here
+// </main>
+// <footer>
+//      every footer related elements can go here
+// </footer>
